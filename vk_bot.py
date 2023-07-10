084ea4663a4b6b72ccc0ad7f3dde000c3a698b5b
Serega <antonovseantonoff@yandex.ru>
2023-07-10 11:54:10 +0300

    Add echobot for VK

--- vk_bot.py
+++ vk_bot.py
@@ -1,20 +1,28 @@
 import vk_api
 from vk_api.longpoll import VkLongPoll, VkEventType
 from environs import Env
+import random
 
 
-env = Env()
-env.read_env()
-telegram_token = env.str('VK_GROUP_TOKEN')
+def echo(event, vk_api_):
 
-vk_session = vk_api.VkApi(token=telegram_token)
+    vk_api_.messages.send(
+        user_id=event.user_id,
+        message=event.text,
+        random_id=random.randint(1, 1000))
 
-longpoll = VkLongPoll(vk_session)
-for event in longpoll.listen():
-    if event.type == VkEventType.MESSAGE_NEW:
-        print('Новое сообщение:')
-        if event.to_me:
-            print('Для меня от: ', event.user_id)
-        else:
-            print('От меня для: ', event.user_id)
-        print('Текст:', event.text)
+
+def main():
+    env = Env()
+    env.read_env()
+    vk_token = env.str('VK_GROUP_TOKEN')
+    vk_session = vk_api.VkApi(token=vk_token)
+    longpoll = VkLongPoll(vk_session)
+    vk_api_ = vk_session.get_api()
+    for event in longpoll.listen():
+        if event.type == VkEventType.MESSAGE_NEW and event.to_me:
+            echo(event, vk_api_)
+
+
+if __name__ == '__main__':
+    main()
