0fcf6927f7fd1b39274d8e0c1d3420eb4400dfc2
Serega <antonovseantonoff@yandex.ru>
2023-07-10 13:32:22 +0300

    Add DialogFlow to VK bot.Reformate code

--- botspeech.py
+++ botspeech.py
@@ -1,29 +1,19 @@
 #!/usr/bin/env python
-# pylint: disable=C0116,W0613
-# This program is dedicated to the public domain under the CC0 license.
-
-"""
-Simple Bot to reply to Telegram messages.
-
-First, a few handler functions are defined. Then, those functions are passed to
-the Dispatcher and registered at their respective places.
-Then, the bot is started and runs until we press Ctrl-C on the command line.
-
-Usage:
-Basic Echobot example, repeats messages.
-Press Ctrl-C on the command line or send a signal to the process to stop the
-bot.
-"""
 
 import logging
+from auxiliary_file import detect_intent_texts
 
-from google.cloud import dialogflow
 from environs import Env
 from telegram import Update
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
+from telegram.ext import (
+    Updater,
+    CommandHandler,
+    MessageHandler, Filters, CallbackContext
+)
 
 logging.basicConfig(
-    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    level=logging.INFO
 )
 
 logger = logging.getLogger(__name__)
@@ -32,6 +22,7 @@ logger = logging.getLogger(__name__)
 def start(update: Update, context: CallbackContext) -> None:
     """Send a message when the command /start is issued."""
     update.message.reply_text('Здравствуйте')
+
 
 def help_command(update: Update, context: CallbackContext) -> None:
     """Send a message when the command /help is issued."""
@@ -44,34 +35,9 @@ def echo(update: Update, context: CallbackContext) -> None:
     env.read_env()
     project_id = env.str('PROJECT_ID')
     session_id = env.str("TELEGRAM_CHAT_ID")
-    answer = detect_intent_texts(project_id, session_id, update.message.text, 'ru-RU')
+    answer = detect_intent_texts(
+        project_id, session_id, update.message.text, 'ru-RU')
     update.message.reply_text(answer)
-
-
-
-def detect_intent_texts(project_id, session_id, text, language_code):
-    """Returns the result of detect intent with texts as inputs.
-    Using the same `session_id` between requests allows continuation
-    of the conversation."""
-
-    session_client = dialogflow.SessionsClient()
-    session = session_client.session_path(project_id, session_id)
-    #print("Session path: {}\n".format(session))
-    text_input = dialogflow.TextInput(text=text, language_code=language_code)
-    query_input = dialogflow.QueryInput(text=text_input)
-    response = session_client.detect_intent(
-        request={"session": session, "query_input": query_input}
-    )
-    print("=" * 20)
-    print("Query text: {}".format(response.query_result.query_text))
-    print(
-        "Detected intent: {} (confidence: {})\n".format(
-            response.query_result.intent.display_name,
-            response.query_result.intent_detection_confidence,
-        )
-    )
-    print("Fulfillment text: {}\n".format(response.query_result.fulfillment_text))
-    return response.query_result.fulfillment_text
 
 
 def main() -> None:
@@ -82,7 +48,8 @@ def main() -> None:
     dispatcher = updater.dispatcher
     dispatcher.add_handler(CommandHandler("start", start))
     dispatcher.add_handler(CommandHandler("help", help_command))
-    dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command, echo))
+    dispatcher.add_handler(MessageHandler(
+        Filters.text & ~Filters.command, echo))
     updater.start_polling()
     updater.idle()
 
--- vk_bot.py
+++ vk_bot.py
@@ -2,26 +2,33 @@ import vk_api
 from vk_api.longpoll import VkLongPoll, VkEventType
 from environs import Env
 import random
+from auxiliary_file import detect_intent_texts
 
 
-def echo(event, vk_api_):
+def echo(event, vk_api_, project_id, session_id):
 
+    answer = detect_intent_texts(project_id, session_id, event.text, 'ru-RU')
     vk_api_.messages.send(
         user_id=event.user_id,
-        message=event.text,
-        random_id=random.randint(1, 1000))
+        message=answer,
+        random_id=random.randint(1, 1000)
+    )
+    answer = detect_intent_texts(project_id, session_id, event.text, 'ru-RU')
 
 
 def main():
     env = Env()
     env.read_env()
     vk_token = env.str('VK_GROUP_TOKEN')
+    project_id = env.str('PROJECT_ID')
+    session_id = env.str('VK_CHAT_ID')
+
     vk_session = vk_api.VkApi(token=vk_token)
     longpoll = VkLongPoll(vk_session)
     vk_api_ = vk_session.get_api()
     for event in longpoll.listen():
         if event.type == VkEventType.MESSAGE_NEW and event.to_me:
-            echo(event, vk_api_)
+            echo(event, vk_api_, project_id, session_id)
 
 
 if __name__ == '__main__':
