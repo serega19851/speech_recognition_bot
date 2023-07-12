e36f20f56a697d59942c8700a8c45d4e96124cce
Serega <antonovseantonoff@yandex.ru>
2023-07-12 13:14:20 +0300

    Sends logs to the bot in telegram

--- botspeech.py
+++ botspeech.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python
-
+from functools import partial
+import telegram
+from time import sleep
+from telegram.error import NetworkError, TelegramError
 import logging
 from auxiliary_file import detect_intent_texts
-
 from environs import Env
 from telegram import Update
 from telegram.ext import (
@@ -29,29 +31,51 @@ def help_command(update: Update, context: CallbackContext) -> None:
     update.message.reply_text('Help!')
 
 
-def echo(update: Update, context: CallbackContext) -> None:
-    """Echo the user message."""
+def sends_response_user(
+    update: Update, context: CallbackContext, project_id
+) -> None:
+    """Sends the user message."""
     env = Env()
     env.read_env()
-    project_id = env.str('PROJECT_ID')
     session_id = env.str("TELEGRAM_CHAT_ID")
-    answer = detect_intent_texts(
-        project_id, session_id, update.message.text, 'ru-RU')
-    update.message.reply_text(answer.fulfillment_text)
+    bot_log = telegram.Bot(token=env.str('TELEGRAM_LOG'))
+    try:
+        answer = detect_intent_texts(
+            project_id, session_id, update.message.text, 'ru-RU')
+        update.message.reply_text(answer.fulfillment_text)
+
+    except NetworkError as network_error:
+        bot_log.send_message(
+            chat_id=session_id,
+            text=f'Ошибка сети tel бота\n{network_error}\n'
+        )
+        sleep(20)
+
+    except TelegramError as telegram_error:
+        bot_log.send_message(
+            chat_id=session_id,
+            text=f'Ошибка телеграм\n{telegram_error}\n'
+        )
 
 
 def main() -> None:
-    env = Env()
-    env.read_env()
-    telegram_token = env.str('TELEGRAM_TOKEN')
-    updater = Updater(telegram_token)
-    dispatcher = updater.dispatcher
-    dispatcher.add_handler(CommandHandler("start", start))
-    dispatcher.add_handler(CommandHandler("help", help_command))
-    dispatcher.add_handler(MessageHandler(
-        Filters.text & ~Filters.command, echo))
-    updater.start_polling()
-    updater.idle()
+    while True:
+
+        env = Env()
+        env.read_env()
+        telegram_token = env.str('TELEGRAM_TOKEN')
+        project_id = env.str('PROJECT_ID')
+
+        updater = Updater(telegram_token)
+        dispatcher = updater.dispatcher
+        dispatcher.add_handler(CommandHandler("start", start))
+        dispatcher.add_handler(CommandHandler("help", help_command))
+        dispatcher.add_handler(MessageHandler(
+            Filters.text & ~Filters.command, partial(
+                sends_response_user, project_id=project_id))
+        )
+        updater.start_polling()
+        updater.idle()
 
 
 if __name__ == '__main__':
--- vk_bot.py
+++ vk_bot.py
@@ -3,12 +3,15 @@ from vk_api.longpoll import VkLongPoll, VkEventType
 from environs import Env
 import random
 from auxiliary_file import detect_intent_texts
+from requests.exceptions import ReadTimeout, ConnectionError
+from time import sleep
+import telegram
 
 
-def echo(event, vk_api_, project_id, session_id):
+def sends_response_user(event, vk_api_, project_id):
+    session_id = f'vk-{event.user_id}'
     answer = detect_intent_texts(
         project_id, session_id, event.text, 'ru-RU')
-
     if not answer.intent.is_fallback:
         vk_api_.messages.send(
             user_id=event.user_id,
@@ -18,18 +21,32 @@ def echo(event, vk_api_, project_id, session_id):
 
 
 def main():
-    env = Env()
-    env.read_env()
-    vk_token = env.str('VK_GROUP_TOKEN')
-    project_id = env.str('PROJECT_ID')
-    session_id = env.str('VK_CHAT_ID')
+    while True:
+        env = Env()
+        env.read_env()
+        vk_token = env.str('VK_GROUP_TOKEN')
+        project_id = env.str('PROJECT_ID')
+        session_te_id = env.str('TELEGRAM_CHAT_ID')
 
-    vk_session = vk_api.VkApi(token=vk_token)
-    longpoll = VkLongPoll(vk_session)
-    vk_api_ = vk_session.get_api()
-    for event in longpoll.listen():
-        if event.type == VkEventType.MESSAGE_NEW and event.to_me:
-            echo(event, vk_api_, project_id, session_id)
+        vk_session = vk_api.VkApi(token=vk_token)
+        longpoll = VkLongPoll(vk_session)
+        vk_api_ = vk_session.get_api()
+        bot_log = telegram.Bot(token=env.str('TELEGRAM_LOG'))
+        try:
+            for event in longpoll.listen():
+                if event.type == VkEventType.MESSAGE_NEW and event.to_me:
+                    sends_response_user(event, vk_api_, project_id)
+        except ReadTimeout as timeout:
+            bot_log.send_message(
+                chat_id=session_te_id,
+                text=f'Ошибка сети VK бота\n{timeout}\n'
+            )
+        except ConnectionError as connect_er:
+            bot_log.send_message(
+                chat_id=session_te_id,
+                text=f'Ошибка VK\n{connect_er}\n'
+            )
+            sleep(20)
 
 
 if __name__ == '__main__':
